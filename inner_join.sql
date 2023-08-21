a2657f9c02dd649ccb3c7fd8e0fe7f1fbd16b7d3
Vitor Tatsumi <108268622+VitorTatsumi@users.noreply.github.com>
2023-08-21 17:57:49 -0300

    Add files via upload

---
+++ inner_join.sql
@@ -0,0 +1,9 @@
+#Com o INNER JOIN é possível exibir mais de um registro de tabelas diferentes
+
+SELECT * FROM users;
+
+SELECT u.id as uid, p.id as pid, u.first_name, p.bio 
+FROM users as u
+INNER JOIN profiles as p
+ON u.id = p.user_id 
+WHERE u.first_name LIKE '%a';
